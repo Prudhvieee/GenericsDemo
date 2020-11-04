ad709f6a96288ffacadbe3442ef758bc22399c97
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-04 09:16:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.2, 1.3, 1.4, 1.5 };
+            toprInt(intArray);
+            toprDouble(doubleArray);
+        }
+        public static void toprInt(int[] inputArray)
+        {
+            foreach (int item in inputArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static void toprDouble(double[] inputArray)
+        {
+            foreach (int item in inputArray)
+            {
+                Console.WriteLine(item+"\n");
+            }
+        }
+
+    }
+}
